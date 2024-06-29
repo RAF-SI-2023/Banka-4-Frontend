747ae3d7faec144db5d0c5bf4c6b5b20ba9ff5ae
Jovan Djukic <jovan@rellify.com>
2024-06-29 12:23:00 +0200

    #246

--- src/zaposleni/components/employeeList.tsx
+++ src/zaposleni/components/employeeList.tsx
@@ -1,9 +1,12 @@
-import {  Table, TableBody, TableRow } from '@mui/material';
+import { Button, Table, TableBody, TableRow } from '@mui/material';
 import { Employee, EmployeeListProps } from '../../utils/types';
 import { ScrollContainer, StyledHeadTableCell, StyledTableCell, StyledTableHead, StyledTableRow } from '../../utils/tableStyles';
 import { useNavigate } from 'react-router-dom';
 import { decodePermissions } from '../../utils/permissions';
 import styled from 'styled-components';
+import { useContext } from 'react';
+import { Context } from 'App';
+import { makeApiRequest } from 'utils/apiRequest';
 
 const StyledTableCellLocal = styled(StyledTableCell)`
   text-align: center!important;
@@ -22,6 +25,11 @@ const EmployeeList: React.FC<EmployeeListProps> = ({ employees }) => {
         const id = event.currentTarget.id;
         navigate(`/izmeniZaposlenog?email=${id}`)
     };
+    const ctx = useContext(Context);
+
+    const handleReset = async (id: string) => {
+        const res = await makeApiRequest(`/radnik/reset-limit/${id}`, 'PUT', {}, false, false, ctx)
+    }
 
     return (
         <ScrollContainer >
@@ -36,6 +44,7 @@ const EmployeeList: React.FC<EmployeeListProps> = ({ employees }) => {
                         <StyledHeadTableCell>Mejl adresa</StyledHeadTableCell>
                         <StyledHeadTableCell>Broj telefona</StyledHeadTableCell>
                         <StyledHeadTableCell>Limit</StyledHeadTableCell>
+                        <StyledHeadTableCell>Reset limit</StyledHeadTableCell>
                         <StyledHeadTableCell>Pozicija</StyledHeadTableCell>
                         <StyledHeadTableCell>Departman</StyledHeadTableCell>
                         <StyledHeadTableCell>Permisije</StyledHeadTableCell>
@@ -52,6 +61,12 @@ const EmployeeList: React.FC<EmployeeListProps> = ({ employees }) => {
                             <StyledTableCell>{employee.email}</StyledTableCell>
                             <StyledTableCell>{employee.brojTelefona}</StyledTableCell>
                             <StyledTableCell>{employee.dailyLimit}</StyledTableCell>
+                            <StyledTableCell>
+                                <Button variant="outlined" color="primary" onClick={(event) => {
+                                    event.stopPropagation();
+                                    handleReset(employee.id)
+                                }}>RESET</Button>
+                            </StyledTableCell>
                             <StyledTableCell>{employee.pozicija}</StyledTableCell>
                             <StyledTableCell>{employee.departman}</StyledTableCell>
                             <StyledTableCellLocal>{decodePermissions(employee.permisije)}</StyledTableCellLocal>
